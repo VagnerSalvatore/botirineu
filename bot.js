42ec0cd604e0cad880e10d0336595b542b1e543f
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 15:36:35 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,5 +49,39 @@ if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
 
 }
                  
- 
+if (message.content.startsWith('!ball')){
+        /**
+ * @member {Object}
+ */
+const snekfetch = require('snekfetch');
+
+exports.run = async (client, msg, args) => {
+    if (!args.join(' ')) {
+        return msg.channel.send('Please include a question');
+    }
+    let r = await snekfetch.get('https://8ball.delegator.com/magic/JSON/0');
+    let answerBall = r.body;
+    /**
+     * @param {{magic:object}} The returned Object from API
+     */
+    let ball = answerBall.magic.answer;
+    msg.channel.send('**[8 Ball]** :crystal_ball: ' + ball);
+};
+
+exports.help = {
+    category: 'fun',
+    usage: '[question]',
+    description: 'Ask the magic 8 ball something',
+    detail: 'Ask the magic 8 ball something',
+    botPerm: ['SEND_MESSAGES'],
+    authorPerm: [],
+    alias: [
+        '8b'
+    ],
+    example: 'Are you the may from overwatch?'
+};
+                
+                
+                
+                
 });
