c70980c51689704ace6806add0d091453dbbf510
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 17:10:29 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,55 +1,56 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const request = require('request');
 
-//Algo Que Conecta O Bot
-bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
-
-        bot.on('message', message => {
-            if (message.content.startsWith('!ping')){
-                message.channel.send('!pong');
-            }
-
-
-
-                if (message.content.startsWith('!on')){
-                    message.channel.send('Claro Que Sim Pô');
-                }
-        
-
-                if (message.content.startsWith('!irineu')){
-                    message.channel.send('Você Não Sabe Nem Eu');
-                }
-                  
-                    if (message.content.startsWith('!netflix')){
-                    message.channel.send('Enviei no Pv');
-                  message.author.sendFile('https://cdn.discordapp.com/attachments/392823534541471765/393060119639752704/logins.txt');
-   
-                           
-                   }
-
-                   if (message.content.startsWith('!testar')){
-                   if (message.channel.sendFile ('http://localhost/logins.txt'));
-                   
-                   }     
-                   
-                  if (message.content.startsWith('!cpf')){
-                   if (message.author.send ("I've just banned you!"));
-                          
-                          
-                  }
-                   
-                
-                
-
-let role = message.guild.roles.find("name", "Admin");
-if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
-    msgDel = 10
-    let numberMessages = parseInt("msgDel")
-    message.channel.fetchMessages({limit: numberMessages}).then(messages => message.channel.bulkDelete(messages));
-
-}
-            
-
-                                
-                
+client.on('ready', () => {
+  console.log(`BOT LIGADO`);
+  client.user.setPresence({ game: { name: `Coded by: Last`, type: 0} });
 });
+
+// Anti chat
+client.on('message',async  (msg) => {
+    var is_PM = msg.channel.type == "dm";
+    var message = msg;
+    if(is_PM && msg.author.id != "393109624078860298"){
+        return msg.reply("**Sem mensagens no privado!**.");
+    }
+
+    if(msg.content.split(" ")[0].indexOf(".") < 0){
+        return;
+    }
+    var cmd = msg.content.split(" ")[0];
+
+ if(cmd == ".teste"){
+    msg.channel.send("**STATUS: ON**");
+ }
+   
+ //lag daporra
+// >
+    //Qualquer comando a bai
+
+    //Ban/Kick
+    if(cmd == ".ban") {
+        let modRole = msg.guild.roles.find("name", "FUNDADOR");
+        if(msg.member.roles.has(modRole.id)) { 
+          let banMember = msg.guild.member(msg.mentions.users.first());
+          msg.guild.member(banMember).ban();
+          msg.channel.sendMessage("Membro **banido** com sucesso :call_me:.");
+        } else {
+          return msg.reply("Você não tem **permissão** de **banir** outros **usuários**.");
+        }
+      }
+
+      if(cmd == ".kick") {
+        let modRole = msg.guild.roles.find("name", "FUNDADOR");
+        if(msg.member.roles.has(modRole.id)) { 
+          let banMember = msg.guild.member(msg.mentions.users.first());
+          msg.guild.member(banMember).kick();
+          msg.channel.sendMessage("Membro **kickado** com sucesso :call_me:.");
+        } else {
+          return msg.reply("Você não tem **permissão** de **kickar** outros **usuários**.");
+        }
+      }
+      //Ban/Kick
+	  
+});
+client.login('MzkzMTA5NjI0MDc4ODYwMjk4.DRxF9A.IHZeIrGJkQzCAQ5kfFC_7UXAnEI'); //Token do seu bot
