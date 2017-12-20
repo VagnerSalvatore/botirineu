d6291231367a84588ee34c101ef631d1dbd43ae0
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 13:04:21 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,31 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-
+const client = new Discord.Client({options: {disabledEvents: [
+    'GUILD_SYNC',
+    'GUILD_ROLE_CREATE',
+    'GUILD_ROLE_DELETE',
+    'GUILD_ROLE_UPDATE',
+    'GUILD_BAN_ADD',
+    'GUILD_BAN_REMOVE',
+    'CHANNEL_CREATE',
+    'CHANNEL_DELETE',
+    'CHANNEL_UPDATE',
+    'CHANNEL_PINS_UPDATE',
+    'MESSAGE_DELETE',
+    'MESSAGE_UPDATE',
+    'MESSAGE_DELETE_BULK',
+    'MESSAGE_REACTION_ADD',
+    'MESSAGE_REACTION_REMOVE',
+    'MESSAGE_REACTION_REMOVE_ALL',
+    'USER_UPDATE',
+    'USER_NOTE_UPDATE',
+    'USER_SETTINGS_UPDATE',
+    'PRESENCE_UPDATE',
+    'VOICE_STATE_UPDATE',
+    'TYPING_START',
+    'VOICE_SERVER_UPDATE',
+    'RELATIONSHIP_ADD',
+    'RELATIONSHIP_REMOVE'
+]}});
 //Algo Que Conecta O Bot
 bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
 
