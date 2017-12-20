e4f2c5731f1c730ba217f14324a4a64aa1d30574
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 17:30:40 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,3 +100,4 @@ if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
 	  
 });
 client.login('MzkyNzk0MTAzNzcyODcyNzE1.DRxHhQ.sYvrQ3k9sCo_LaUPpPAT4hLTXQk'); //Token do seu bot
+});
