ab2844bedcf3f1c6848158eacc778ae4d7a1e88f
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 17:40:47 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,28 +10,35 @@ bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
             }
 
 
-
+              //Ligado ?
                 if (message.content.startsWith('!on')){
                     message.channel.send('Claro Que Sim Pô');
                 }
         
 
+                //Down
+                
                 if (message.content.startsWith('!irineu')){
                     message.channel.send('Você Não Sabe Nem Eu');
                 }
                   
+                
+                //Doação Netflix
                     if (message.content.startsWith('!netflix')){
                     message.channel.send('Enviei no Pv');
                   message.author.sendFile('https://cdn.discordapp.com/attachments/392823534541471765/393060119639752704/logins.txt');
    
                            
                    }
-
+                 
+                //Testador De Logins
                    if (message.content.startsWith('!testar')){
                    if (message.channel.sendFile ('http://localhost/logins.txt'));
                    
                    }     
                    
+                
+                //Ira Puxar o Cpf
                   if (message.content.startsWith('!cpf')){
                    if (message.author.send ("I've just banned you!"));
                           
@@ -40,8 +47,8 @@ bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
                    
                 
                 
-
-let role = message.guild.roles.find("name", "Admin");
+//Deletando Mensagens
+let role = message.guild.roles.find("name", "DONO");
 if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
     msgDel = 10
     let numberMessages = parseInt("msgDel")
