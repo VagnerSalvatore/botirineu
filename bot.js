01e55ecf05b396f5335bc9a61f44020dc1f993f4
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 17:37:17 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,56 +1,10 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const request = require('request');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`BOT LIGADO`);
-  client.user.setPresence({ game: { name: `Irineu`, type: 0} });
-});
+//Algo Que Conecta O Bot
+bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
 
-// Anti chat
-client.on('message',async  (msg) => {
-    var is_PM = msg.channel.type == "dm";
-    var message = msg;
-    if(is_PM && msg.author.id != "392794103772872715"){
-        return msg.reply("**Sem mensagens no privado!**.");
-    }
-
-    if(msg.content.split(" ")[0].indexOf(".") < 0){
-        return;
-    }
-    var cmd = msg.content.split(" ")[0];
-
- if(cmd == ".teste"){
-    msg.channel.send("**STATUS: ON**");
- }
-   
-    //Comandos Bot
-
-    //Ban/Kick
-    if(cmd == "!ban") {
-        let modRole = msg.guild.roles.find("name", "DONO");
-        if(msg.member.roles.has(modRole.id)) { 
-          let banMember = msg.guild.member(msg.mentions.users.first());
-          msg.guild.member(banMember).ban();
-          msg.channel.sendMessage("Membro **banido** com sucesso :call_me:.");
-        } else {
-          return msg.reply("Sem Permissão :vindieselpngmemevindieselpng512:.");
-        }
-      }
-
-      if(cmd == "!kick") {
-        let modRole = msg.guild.roles.find("name", "DONO");
-        if(msg.member.roles.has(modRole.id)) { 
-          let banMember = msg.guild.member(msg.mentions.users.first());
-          msg.guild.member(banMember).kick();
-          msg.channel.sendMessage("Membro **kickado** com sucesso :call_me:.");
-        } else {
-          return msg.reply("Sem Permissão :vindieselpngmemevindieselpng512:.");
-        }
-      }
-      //Ban/Kick
-	
-	//Comandos Por Dark
+        bot.on('message', message => {
             if (message.content.startsWith('!ping')){
                 message.channel.send('!pong');
             }
@@ -94,8 +48,8 @@ if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
     message.channel.fetchMessages({limit: numberMessages}).then(messages => message.channel.bulkDelete(messages));
 
 }
-	
-	
-	  
+            
+
+                                
+                
 });
-client.login('MzkyNzk0MTAzNzcyODcyNzE1.DRxHhQ.sYvrQ3k9sCo_LaUPpPAT4hLTXQk'); //Token do seu bot
