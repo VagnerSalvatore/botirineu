48d58c1ff52faf2b32946d252d82b859b2b23fef
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 14:32:38 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,6 +49,51 @@ if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
 
 }
                 
+                
+                
+                }
+
+client.on(`message`, message => {
+    if (message.content === "!ban") {
+      
+    }
+  });
+
+  client.on(`message`, message => {
+    if (message.content === "!kick") {
+      let modRole = message.guild.roles.find("name", "Moderators");
+      if(message.member.roles.has(modRole.id)) { 
+         
+      }
+    }
+  });
+
+  client.on(`message`, message => {
+    if (message.content === "!kick") {
+      let modRole = message.guild.roles.find("name", "Moderators");
+      if(message.member.roles.has(modRole.id)) { 
+        let kickMember = message.guild.member(message.mentions.users.first());
+      }
+    }
+  });
+
+  client.on(`message`, message => {
+    if (message.content === "!kick") {
+      let modRole = message.guild.roles.find("name", "Moderators");
+      if(message.member.roles.has(modRole.id)) { 
+        let kickMember = message.guild.member(message.mentions.users.first());
+        message.guild.member(kickMember).kick();
+        message.channel.sendMessage("Member Kicked.");
+      } else {
+        return message.reply("You dont have the perms to kick members. scrub.");
+      }
+    }
+  });
+
+                
+                
+                
+                
 
 
 });
