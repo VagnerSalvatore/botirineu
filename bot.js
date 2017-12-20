0af6b0dfac14de8eadc429b142f1d3ede0998de7
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-20 11:25:20 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-
+//Algo Que Conecta O Bot
+client.login(process.env.BOT_TOKEN);
 
 bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
 
