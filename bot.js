d2ad862d7d8a1e9d2086c6074094d98ce265a4ab
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-21 13:53:25 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,67 +3,69 @@ const bot = new Discord.Client();
 //Algo Que Conecta O Bot
 bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
 
-        bot.on('message', message => {
-            if (message.content.startsWith('!ping')){
-                message.channel.send('!pong');
+bot.on('message', message => {
+    if (message.content.startsWith('!ping')) {
+        message.channel.send('!pong');
+    }
+
+
+    //Ligado ?
+    if (message.content.startsWith('!on')) {
+        message.channel.send('Claro Que Sim Pô');
+    }
+
+
+    //Down
+
+    if (message.content.startsWith('!irineu')) {
+        message.channel.send('Você Não Sabe Nem Eu');
+    }
+
+
+    //Doação Netflix
+    if (message.content.startsWith('!netflix')) {
+        message.channel.send('Enviei no Pv');
+        message.author.sendFile('https://cdn.discordapp.com/attachments/392823534541471765/393060119639752704/logins.txt');
+
+
+    }
+
+    //Testador De Logins
+    if (message.content.startsWith('!testar')) {
+        if (message.channel.sendFile('http://localhost/logins.txt'));
+
+    }
+
+
+    //Ira Puxar o Cpf
+    if (message.content.startsWith('!cpf')) {
+        if (message.author.send("I've just banned you!"));
+
+    }
+
+    if (message.content.startsWith('!info')) {
+        message.channel.send({
+            embed: {
+                color: 3447003,
+                description: "A very simple Embed!"
             }
-            
+        });
+    }
+    if (message.content.startsWith('!invite')) {
+        message.guild.channels.get('392823534541471765').createInvite().then(invite =>
+            message.channel.send(invite.url)
+        );
+    }
 
-              //Ligado ?
-                if (message.content.startsWith('!on')){
-                    message.channel.send('Claro Que Sim Pô');
-                }
-        
+    //Deletando Mensagens
+    let role = message.guild.roles.find("name", "DONO");
+    if (message.member.roles.has(role.id) && message.content.startsWith("!delete")) {
+        msgDel = 10
+        let numberMessages = parseInt("msgDel")
+        message.channel.fetchMessages({ limit: numberMessages }).then(messages => message.channel.bulkDelete(messages));
 
-                //Down
-                
-                if (message.content.startsWith('!irineu')){
-                    message.channel.send('Você Não Sabe Nem Eu');
-                }
-                  
-                
-                //Doação Netflix
-                    if (message.content.startsWith('!netflix')){
-                    message.channel.send('Enviei no Pv');
-                  message.author.sendFile('https://cdn.discordapp.com/attachments/392823534541471765/393060119639752704/logins.txt');
-   
-                           
-                   }
-                 
-                //Testador De Logins
-                   if (message.content.startsWith('!testar')){
-                   if (message.channel.sendFile ('http://localhost/logins.txt'));
-                   
-                   }     
-                   
-                
-                //Ira Puxar o Cpf
-                  if (message.content.startsWith('!cpf')){
-                   if (message.author.send ("I've just banned you!"));      
-                          
-                  }
-                   
-                  if (message.content.startsWith('!info')){
-                    message.channel.send({embed: {
-                        color: 3447003,
-                        description: "A very simple Embed!"
-                      }});
-                    }
-                                   if(message.content.startsWith('!invite')){
-                        message.guild.channels.get('392823534541471765').createInvite().then(invite =>
-                            message.channel.send(invite.url)
-                        );
-                    }
-                    
-//Deletando Mensagens
-let role = message.guild.roles.find("name", "DONO");
-if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
-    msgDel = 10
-    let numberMessages = parseInt("msgDel")
-    message.channel.fetchMessages({limit: numberMessages}).then(messages => message.channel.bulkDelete(messages));
-    
-}
-            
-             
-                
+    }
+
+
+
 });
