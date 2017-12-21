b2ab0a4eb1a29670d8f82500300c58430527447d
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-21 20:13:57 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const embed = new Discord.RichEmbed();
 const client = new Discord.Client();
