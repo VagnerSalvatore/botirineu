fecdac8e83fbdbeb05c28d6c0db45bd3047585e3
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-21 14:08:34 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-//LOGIN DO BOT
+const embed = new Discord.RichEmbed();
+//Login Do Bot
 bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
 
 bot.on('message', message => {
@@ -43,11 +44,11 @@ bot.on('message', message => {
 
     }
 
-    if (message.content.startsWith('!info')) {
+    if (message.content.startsWith('!help')) {
         message.channel.send({
             embed: {
                 color: 3447003,
-                description: "A very simple Embed!"
+                description: "Meus Comandos"
             }
         });
     }
@@ -56,6 +57,39 @@ bot.on('message', message => {
             message.channel.send(invite.url)
         );
     }
+    if (message.content.startsWith('!info')) {
+        const embed = new Discord.RichEmbed()
+            .setTitle("This is your title, it can hold 256 characters")
+            .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
+            /*
+             * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
+             */
+            .setColor(0x00AE86)
+            .setDescription("This is the main body of text, it can hold 2048 characters.")
+            .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
+            .setImage("http://i.imgur.com/yVpymuV.png")
+            .setThumbnail("http://i.imgur.com/p2qNFag.png")
+            /*
+             * Takes a Date object, defaults to current date.
+             */
+            .setTimestamp()
+            .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
+            .addField("This is a field title, it can hold 256 characters",
+            "This is a field value, it can hold 2048 characters.")
+            /*
+             * Inline fields may not display as inline if the thumbnail and/or image is too big.
+             */
+            .addField("Inline Field", "They can also be inline.", true)
+            /*
+             * Blank field, useful to create some space.
+             */
+            .addBlankField(true)
+            .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
+
+        message.channel.send({ embed });
+
+    }
+
 
     //Deletando Mensagens
     let role = message.guild.roles.find("name", "DONO");
