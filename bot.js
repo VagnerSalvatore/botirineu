8a6163ae0e2ba2a3c14ad7f434a167d866030739
VagnerSalvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-21 13:41:24 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,35 +45,9 @@ bot.login('MzkyNzk0MTAzNzcyODcyNzE1.DRtKpg.do0s2qG6k8oXSeLmVajwh8dnuJ8');
                    
                   if (message.content.startsWith('!info')){
                     message.channel.send({embed: {
-    color: 3447003,
-    author: {
-      name: client.user.username,
-      icon_url: client.user.avatarURL
-    },
-    title: "This is an embed",
-    url: "http://google.com",
-    description: "This is a test embed to showcase what they look like and what they can do.",
-    fields: [{
-        name: "Fields",
-        value: "They can have different fields with small headlines."
-      },
-      {
-        name: "Masked links",
-        value: "You can put [masked links](http://google.com) inside of rich embeds."
-      },
-      {
-        name: "Markdown",
-        value: "You can put all the *usual* **__Markdown__** inside of them."
-      }
-    ],
-    timestamp: new Date(),
-    footer: {
-      icon_url: client.user.avatarURL,
-      text: "© Example"
-    }
-  }
-});
-                      
+                        color: 3447003,
+                        description: "A very simple Embed!"
+                      }});
                     }
                                    if(message.content.startsWith('!invite')){
                         message.guild.channels.get('392823534541471765').createInvite().then(invite =>
