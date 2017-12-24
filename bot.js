adcf717519598df4fd1a785f5284cbf72bf3996c
Vagner Salvatore <34711840+VagnerSalvatore@users.noreply.github.com>
2017-12-23 23:34:13 -0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const embed = new Discord.RichEmbed();;
-// Global Settings
+const embed = new Discord.RichEmbed();
+
 
 const prefix = '!'; //PREFIXO DO BOT.
 //Login Do Bot
@@ -11,7 +11,6 @@ bot.on('ready', () => {
     console.log(`IRINEU ON`);
     bot.user.setPresence({ game: { name: `Look At Me!`, type: 0 } });
 });
-
 // Anti chat
 bot.on('message', async (msg) => {
     var is_PM = msg.channel.type == "dm";
@@ -29,40 +28,49 @@ bot.on('message', async (msg) => {
         msg.channel.send("**Claro Que Sim Pô**");
     }
 
+      //Bem Vindo
+      bot.on("guildMemberAdd", function (member) {
+        const WelcomeMsg = new Discord.RichEmbed()
+            .setTitle("Novo Integrante!")
+            .setColor(0x42f1f4)
+            .setDescription(member.toString() + "**Bem Vindo Ao Servidor** :wave: ")
+            .setFooter("Joined the server at!")
+            .setTimestamp();
 
-    //Down
+        var channel = bot.channels.get("393749252896981003");
+        member.addRole("393750458432356353");
+        channel.send({ embed: WelcomeMsg })
+      });
 
-    if (message.content.startsWith('!irineu')) {
-        message.channel.send('Você Não Sabe Nem Eu');
-    }
+                if (message.content.startsWith('!irineu')){
+                    message.channel.send('Você Não Sabe Nem Eu');
+                }
+                  
+                    if (message.content.startsWith('!netflix')){
+                    message.channel.send('Enviei no Pv');
+                  message.author.sendFile('https://cdn.discordapp.com/attachments/392823534541471765/393060119639752704/logins.txt');
+   
+                           
+                   }
 
-
-    //Doação Netflix
-    if (message.content.startsWith('!netflix')) {
-        message.channel.send('Enviei no Pv');
-        message.author.sendFile('https://cdn.discordapp.com/attachments/392823534541471765/393060119639752704/logins.txt');
-
-
-    }
-
-    //KKK
+                  //KKK
     if (message.content.startsWith('!bom dia')) {
 
         message.channel.send("Bom Dia " + ", É o Caralho!" + message.author.toString());
-    }
-
-
-    //Numero de Servidores
+    }  
+                   
+     //Numero de Servidores
     if (message.content.startsWith('!bot')) {
         message.channel.send('Servidores Em Que Estou');
         message.channel.send(bot.guilds.size)
     }
+                
     if (message.content.startsWith('!invite')) {
         message.guild.channels.get('393841445376622612').createInvite().then(invite =>
            message.channel.send(invite.url)
-        );
-        }
-    
+        )
+    }
+
     if (message.content.startsWith('!botinfo')) {
         const embed = new Discord.RichEmbed()
             .setTitle("Irineubot.com.br")
@@ -96,102 +104,82 @@ bot.on('message', async (msg) => {
 
     }
 
+
     if (message.content.startsWith("!ping")) {
         const embed = new Discord.RichEmbed()
         message.channel.send(new Date().getTime() - message.createdTimestamp + " ms :ping_pong: pong");
 
     }
 
-    //Ban/Kick
-    if (cmd == "!ban") {
-        let modRole = msg.guild.roles.find("name", "Dono");
-        if (msg.member.roles.has(modRole.id)) {
-            let banMember = msg.guild.member(msg.mentions.users.first());
-            msg.guild.member(banMember).ban();
-            msg.channel.sendMessage("Membro **banido** com sucesso :call_me:.");
-     } else {
-            return msg.reply("Sem Permissão! :face_palm:.");
+//Ban/Kick
+if (cmd == "!ban") {
+    let modRole = msg.guild.roles.find("name", "Dono");
+    if (msg.member.roles.has(modRole.id)) {
+        let banMember = msg.guild.member(msg.mentions.users.first());
+        msg.guild.member(banMember).ban();
+        msg.channel.sendMessage("Membro **banido** com sucesso :call_me:.");
+ } else {
+        return msg.reply("Sem Permissão! :face_palm:.");
+}
+}
+
+if (cmd == "!kick") {
+    let modRole = msg.guild.roles.find("name", "Dono");
+    if (msg.member.roles.has(modRole.id)) {
+        let banMember = msg.guild.member(msg.mentions.users.first());
+        msg.guild.member(banMember).kick();
+        msg.channel.sendMessage("Membro **Chutado** com sucesso :call_me:.");
+    } else {
+        return msg.reply("Sem Permissão! :face_palm:.");
     }
+}
 
-    if (cmd == "!kick") {
-        let modRole = msg.guild.roles.find("name", "Dono");
-        if (msg.member.roles.has(modRole.id)) {
-            let banMember = msg.guild.member(msg.mentions.users.first());
-            msg.guild.member(banMember).kick();
-            msg.channel.sendMessage("Membro **Chutado** com sucesso :call_me:.");
-        } else {
-            return msg.reply("Sem Permissão! :face_palm:.");
-        }
-        //Ban/Kick
-    }
-
-
-    //Deletando Mensagens
-    if(message.content.startsWith('!delete')){
-        let role = message.guild.roles.find("name", "Dono");
-        if (message.member.roles.has(role.id)) {
-            msgDel = 10
-            let numberMessages = parseInt("msgDel")
-            message.channel.fetchMessages({ limit: numberMessages }).then(messages => message.channel.bulkDelete(messages));
-        } else {
-            return msg.reply("Sem Permissão! :face_palm:.");
-        }
-
-    }
-
-//DANDO CARGO A USUARIOS
+    
 let role = message.guild.roles.find("name", "Dono");
-    if (message.member.roles.has(role.id)) {
-        //MENCIONAR USUARIO
-        let member = message.mentions.members.first();
-        if (message.content.startsWith('!cargo')) {
-            member.addRole(role).catch(console.error);
-        }
-    }
-    //MECIONAR USUARIO
-    if (message.content.startsWith('!removecargo')) {
-        member.removeRole(role).catch(console.error);
+if(message.member.roles.has(role.id) && message.content.startsWith("!delete")){
+    msgDel = 10
+    let numberMessages = parseInt("msgDel")
+    message.channel.fetchMessages({limit: numberMessages}).then(messages => message.channel.bulkDelete(messages));
 
-    }
+}
+                
 
-    //SISTEMA DE PONTOS
+//SISTEMA DE PONTOS
+const Discord = require("discord.js");
+const fs = require("fs");
+const client = new Discord.Client();
 
-    const Discord = require("discord.js");
-    const fs = require("fs");
-    const client = new Discord.Client();
+let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+const prefix = "!";
 
-    let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+client.on("message", message => {
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
 
-    client.on("message", message => {
-        if (!message.content.startsWith(prefix)) return;
-        if (message.author.bot) return;
+  if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+    level: 0
+  };
+  let userData = points[message.author.id];
+  userData.points++;
 
-        if (!points[message.author.id]) points[message.author.id] = {
-            points: 0,
-            level: 0
-        };
-        let userData = points[message.author.id];
-        userData.points++;
+  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+  if (curLevel > userData.level) {
+    // Level up!
+    userData.level = curLevel;
+    message.reply(`Você Subiu Para O Nivel **${curLevel}**! é isso mesmo irineu?`);
+  }
 
-        let curLevel = Math.floor(1.1 * Math.sqrt(userData.points));
-        if (curLevel > userData.level) {
-            // Level up!
-            userData.level = curLevel;
-            message.reply(`Você Subiu Para O Nivel **${curLevel}**! Não é isso irineu?`);
-        }
+  if (message.content.startsWith(prefix + "level")) {
+    message.reply(`Seu Nivel É ${userData.level}, Com ${userData.points} Pontos.`);
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  });
 
-        if (message.content.startsWith(prefix + "level")) {
-            message.reply(`Seu Nivel É ${userData.level}, Com ${userData.points} Pontos.`);
-        }
-        fs.writeFile("./points.json", JSON.stringify(points), (err) => {
-            if (err) console.error(err)
-        });
+});
 
-    });
+client.login("MzkyNzk0MTAzNzcyODcyNzE1.DR8FZg.OJFtzwGysBBCUYrp9MxaXxW2htA");
 
-    client.login("MzkyNzk0MTAzNzcyODcyNzE1.DR8FZg.OJFtzwGysBBCUYrp9MxaXxW2htA");
-
-
-    }
 
 });
